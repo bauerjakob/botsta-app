fdc01adf86a5b11cfd85fc5857f68cd3ba5fe9c9
Jakob Bauer <mail@bauer-jakob.de>
2021-05-13 17:00:40 +0200

    * Extract app_bar_action_buton into own widget * Added libary full screen modal dialogs

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,10 +1,13 @@
 import 'package:botsta_app/config/routes/routes_config.dart';
 import 'package:botsta_app/logic/bloc/chatroom_bloc.dart';
 import 'package:botsta_app/models/chatroom.dart';
+import 'package:botsta_app/widgets/app_bar_action_button.dart';
 import 'package:botsta_app/widgets/widgets.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:botsta_app/utils/extentions/context_extentions.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -26,18 +29,18 @@ class HomeScreen extends StatelessWidget {
               right: 25,
             ),
             width: 58,
-            child: Material(
-              color: context.theme().accentColor,
-              borderRadius: BorderRadius.circular(13),
-              child: InkWell(
-                borderRadius: BorderRadius.circular(13),
-                child: Icon(
+            child: AppBarActionButton(
+                Icon(
                   Icons.add,
                   size: 30,
-                ),
-                onTap: () {},
-              ),
-            ),
+                ), onTap: () {
+              showCupertinoModalBottomSheet(
+                  expand: true,
+                  context: context,
+                  builder: (context) {
+                    return Container();
+                  });
+            }),
           ),
           // IconButton(
           //   icon: Icon(Icons.settings, color: Colors.black,),
---
+++ lib/widgets/app_bar_action_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:botsta_app/utils/extentions/context_extentions.dart';
+
+class AppBarActionButton extends StatelessWidget {
+  final Icon icon;
+  final Function onTap;
+
+  const AppBarActionButton(this.icon, { required this.onTap });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: context.theme().accentColor,
+      borderRadius: BorderRadius.circular(13),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(13),
+        child: icon,
+        onTap: () => onTap(),
+      ),
+    );
+  }
+}
